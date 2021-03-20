53fed377ba036f6f8b01fda94b254b8079494415
小偉哥 <erichuang@EricMBP.local>
2021-03-20 20:37:25 +0800

    separate top/bottom part

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ const material = {
       title: "新增旅遊套票",
     },
     options: [
-      { name: "套票名稱", hint: "請填寫套票名稱" },
-      { name: "圖片網址", hint: "請填寫圖片網址" },
-      { name: "景點地區", hint: "請選擇景點地區" },
-      { name: "套票金額", hint: "請填寫套票金額" },
-      { name: "套票組數", hint: "請填寫套票組數" },
-      { name: "套票星級", hint: "請填寫套票星級" },
-      { name: "套票描述", hint: "請填寫套票描述" },
+      { title: "套票名稱", hint: "請填寫套票名稱" },
+      { title: "圖片網址", hint: "請填寫圖片網址" },
+      { title: "景點地區", hint: "請選擇景點地區" },
+      { title: "套票金額", hint: "請填寫套票金額" },
+      { title: "套票組數", hint: "請填寫套票組數" },
+      { title: "套票星級", hint: "請填寫套票星級" },
+      { title: "套票描述", hint: "請填寫套票描述" },
     ],
   },
 };
@@ -29,22 +29,44 @@ function addLogo() {
   dom.innerHTML += `
 			<img src="${pic}" class="logo-pic">
 			<img src="${title}" class="logo-title">
-    
     `;
 }
 
-function addOptions() {
+function addTicketOptions() {
   const dom = document.querySelector(".ticket-container");
   const { header, options } = material.ticketInfo;
+
+  function createOptionList() {
+    // options.title, hint
+    let optList = "";
+    options.forEach(({ title, hint }) => {
+      optList += `
+        <div class="ticket-input-container">
+          <span class="ticket-title">${title}</span>
+          <div class="ticket-input-block">
+            <input value=${hint} class="ticket-input">
+          </div>
+        </div>
+      `;
+    });
+
+    // console.log(optList);
+    return optList;
+  }
 
   let htmlMsg = "";
   // add header
   const { icon, title } = header;
+
   htmlMsg += `
 		<div class=${"ticket-header"}>
 			<span class=${"ticket-btn-new"}>${icon}</span>
 			<span class=${"ticket-title"}>${title}</span>
 		</div>
+
+    <div class=${"ticket-content"}>
+      ${createOptionList()}
+    </div>
 	`;
 
   dom.innerHTML += htmlMsg;
@@ -52,4 +74,4 @@ function addOptions() {
 
 // === main ===
 addLogo();
-addOptions();
+addTicketOptions();
