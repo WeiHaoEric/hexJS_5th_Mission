23a0c6bce1ea94c21fa718a683386a7fdd409e85
小偉哥 <erichuang@EricMBP.local>
2021-03-20 16:16:21 +0800

    create dom-start

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.export = {
+  printWidth: 80,
+  tabWidth: 4,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  parser: "babylon",
+};
---
+++ main.js
@@ -0,0 +1,7 @@
+const material = {
+  mainPic: "https://i.imgur.com/iRssPpJ.png",
+  mainLogo: "https://i.imgur.com/vJt0znH.png",
+};
+
+const domStart = document.querySelector(".dom-start");
+console.log(domStart);
