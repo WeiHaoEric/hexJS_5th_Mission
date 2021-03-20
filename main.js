c54469b080720ee8321c35ce8ed78dfc138fc5df
小偉哥 <erichuang@EricMBP.local>
2021-03-20 16:21:52 +0800

    add logo

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const material = {
-  mainPic: "https://i.imgur.com/iRssPpJ.png",
-  mainLogo: "https://i.imgur.com/vJt0znH.png",
+  logo: {
+    pic: "https://i.imgur.com/iRssPpJ.png",
+    title: "https://i.imgur.com/vJt0znH.png",
+  },
 };
 
+function addLogo(dom) {
+  const { pic, title } = material.logo;
+
+  dom.innerHTML = `
+    <img src="${pic}">
+    <img src="${title}">
+    `;
+}
+
 const domStart = document.querySelector(".dom-start");
+addLogo(domStart);
 console.log(domStart);
